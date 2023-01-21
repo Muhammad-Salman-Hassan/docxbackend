f5a2b6127ad890803b31731647c5cf1c17aafe89
unknown <msalmanhassan288311@gmail.com>
2023-01-21 21:12:09 +0500

    fixing issue

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { DataTypes } = require("sequelize");
-const { sequelize} = require("../models");
+const { sequelize } = require("../models");
 // const User = require("../models/User")(sequelize, DataTypes);
 
 const bcrypt = require("bcrypt");
@@ -13,16 +13,15 @@ const sendEmail = require("../services/sendEmail");
 const crypto = require("crypto");
 const db = require("../models");
 const { json } = require("body-parser");
-const User=db.user
-const Profile=db.userProfile
+const User = db.user;
+const Profile = db.userProfile;
 
-var privatekey =
-  "HadZrLuLUpmDcWjz5Vpc04LIopvOQsChok73LQqvs8UWapnH8j3rcHAlfpX";
+var privatekey = "HadZrLuLUpmDcWjz5Vpc04LIopvOQsChok73LQqvs8UWapnH8j3rcHAlfpX";
 
 router.post("/register", async (req, res) => {
   const user = req.body;
-  
-  let Existuser = await User.findOne({where:{email:user.email}});
+
+  let Existuser = await User.findOne({ where: { email: user.email } });
   if (Existuser) {
     throw new Error("Email already exist");
   }
@@ -53,91 +52,109 @@ router.post("/login", async (req, res) => {
       expiresIn: "1d",
     });
 
-
     res.cookie("accessToken", accessToken, {
       httpOnly: true,
-      sameSite: 'none',
+      sameSite: "none",
       secure: true,
       // secure: process.env.NODE_ENV === "production", for Production
-      maxAge: 2 * 60 * 60 * 1000
+      maxAge: 2 * 60 * 60 * 1000,
     });
     // res.setHeader('accessToken', accessToken);
-    res.status(200).json({ cnic, accessToken ,isAuth:true});
+    res.status(200).json({ cnic, accessToken, isAuth: true });
   });
 });
 
-router.get('/logout', async(req, res) => {
-  
+router.get("/logout", async (req, res) => {
   res.clearCookie("accessToken");
-  res.json({msg:"Logout",isAuth:false})
-})
+  res.json({ msg: "Logout", isAuth: false });
+});
 
 //////////Reset Password Routes
 // let verifiedEmail
 router.post("/forgetpassword", async (req, res) => {
   const useremail = req.body.email;
-  console.log(useremail)
- let verifiedEmail = await User.findOne({ where: { email: useremail } });
+  console.log(useremail);
+  let verifiedEmail = await User.findOne({ where: { email: useremail } });
   // console.log(verifiedEmail)
   if (!verifiedEmail) {
-    return res.json({ error: "Email Not Found In Our DataBase Record" ,code:401});
+    return res.json({
+      error: "Email Not Found In Our DataBase Record",
+      code: 401,
+    });
   }
-  const {id,password,email,name} = verifiedEmail 
-  const payload={
+  const { id, password, email, name } = verifiedEmail;
+  const payload = {
     id,
     email,
-   
+  };
+  console.log(verifiedEmail);
 
-  }
-  console.log(verifiedEmail)
- 
- const secret=privatekey+password
+  const secret = privatekey + password;
 
- const token=jwt.sign(payload,secret,{expiresIn:"15m"})
+  const token = jwt.sign(payload, secret, { expiresIn: "15m" });
 
- const link=`http://localhost:3000/reset-password/${id}/${token}`
- console.log(token)
-// console.log(verifiedEmail)
-sendEmail(email,token,link,name)
-res.json({msg:"We have Sent You Email Please Check It Out!",code:200}).status(200)
+  const link = `http://localhost:3000/reset-password/${id}/${token}`;
+  console.log(token);
+  // console.log(verifiedEmail)
+  sendEmail(email, token, link, name);
+  res
+    .json({ msg: "We have Sent You Email Please Check It Out!", code: 200 })
+    .status(200);
 });
 
-router.get("/reset-password/:id/:token",async(req,res,next)=>{
-  const {id,token}=req.params
-  console.log(token)
+router.get("/reset-password/:id/:token", async (req, res, next) => {
+  const { id, token } = req.params;
+  // console.log(token);
   let verifiedEmail = await User.findOne({ where: { id: id } });
-  const {password,email, name} = verifiedEmail
-  
-  const secret=privatekey+password
+  const { password, email, name } = verifiedEmail;
+
+  const secret = privatekey + password;
 
   try {
-    const payload=jwt.verify(token,secret)
-    res.send({email, name})
+    const payload = jwt.verify(token, secret);
+    res.send({ email, name });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-})
+});
 
-router.post("/reset-password/:id/:token",async(req,res,next)=>{
-  const {id,token}=req.params
-const {userpassword,confirmPassword}=req.body
-const user = await User.findOne({ where: { id: id } });
+router.post("/reset-password/:id/:token", async (req, res, next) => {
+  const { id, token } = req.params;
+  const { userpassword, confirmPassword } = req.body;
+  const user = await User.findOne({ where: { id: id } });
+  console.log(user.password)
+  console.log(userpassword,confirmPassword)
+  if (userpassword !== confirmPassword) {
+    return res.json({ msg: "Password Does not Match" });
+  }
+
+  const secret = privatekey + user.password;
   
-  const secret=privatekey+user.password
-
+  
   try {
-    const payload=jwt.verify(token,secret)
-    user.password=userpassword
-    res.send("password change succesfully")
-    
+    const payload = jwt.verify(token, secret);
+    const salt = await bcrypt.genSaltSync(10, "a");
+    console.log(payload)
+    if (payload!==undefined) {
+      // user.password = bcrypt.hashSync(userpassword, salt);
+      await User.update({
+        password: bcrypt.hashSync(userpassword, salt),
+        
+      },
+      {
+        where: {
+          email: payload.email
+        }
+      });
+      res.send("password change succesfully");
+      console.log(user.password)
+    }
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-})
-
+});
 
 router.get("/dashboard", auth, async (req, res) => {
-  
   let cnic = (req.id = req.user);
   console.log(cnic);
 
@@ -147,18 +164,20 @@ router.get("/dashboard", auth, async (req, res) => {
       id: cnic.id,
     },
   });
-  
-  res.json({Message:"Verified",singleuser})
+
+  res.json({ Message: "Verified", singleuser });
 });
 
-router.get("/userprofile", auth,async (req, res) => {
- 
+router.get("/userprofile", auth, async (req, res) => {
   let cnic = (req.id = req.user);
   console.log(cnic);
-  
-  const users = await User.findOne({ include: Profile,where:{id:cnic.id}});
-  
-  res.json(users)
+
+  const users = await User.findOne({
+    include: Profile,
+    where: { id: cnic.id },
+  });
+
+  res.json(users);
 });
 
 module.exports = router;
